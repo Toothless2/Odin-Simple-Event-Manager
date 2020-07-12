d1177d84d42730dc56e778ee2a3bf4afce436282
Toothless2 <max.rose366@gmail.com>
2020-07-12 12:59:43 +0100

    fixed typo

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
     belongs_to :owner, class_name: 'User'
     has_many :users_going_to_events, dependent: :destroy
     has_many :users, through: :users_going_to_events
-    validates :title, :description, :when, presence: true\
+    validates :title, :description, :when, presence: true
 
     # this should be a query but it dosnt like it
     def Event.present
